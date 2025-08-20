16e9288aee89de1c2983bb2bab9de8970cba5ac1
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-08-21 00:51:55 +0300

    Создание проекта: Spring Initializr

---
+++ src/main/java/ru/abasov/StockMaster/StockMasterApplication.java
@@ -0,0 +1,13 @@
+package ru.abasov.StockMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/abasov/StockMaster/StockMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abasov.StockMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
