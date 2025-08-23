4afb9c1487d6dd7786c93787be34de82285b442b
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-08-24 00:35:52 +0300

    Создание общего обработчика исключений для всех контроллеров catalogue-service

--- catalogue-service/src/main/java/ru/abasov/catalogue/controller/ProductRestController.java
+++ catalogue-service/src/main/java/ru/abasov/catalogue/controller/ProductRestController.java
@@ -2,18 +2,14 @@ package ru.abasov.catalogue.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceResolvable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.abasov.catalogue.controller.payload.UpdateProductPayload;
 import ru.abasov.catalogue.entity.Product;
 import ru.abasov.catalogue.service.ProductService;
 
-import java.util.Locale;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -22,7 +18,6 @@ import java.util.NoSuchElementException;
 public class ProductRestController {
 
     private final ProductService productService;
-    private final MessageSource messageSource;
 
     @ModelAttribute("product")
     public Product getProduct(@PathVariable("productId") int productId) {
@@ -38,18 +33,13 @@ public class ProductRestController {
     @PatchMapping
     public ResponseEntity<?> updateProduct(@PathVariable("productId") int productId,
                                            @Valid @RequestBody UpdateProductPayload payload,
-                                           BindingResult bindingResult,
-                                           Locale locale) {
+                                           BindingResult bindingResult) throws BindException {
         if (bindingResult.hasErrors()) {
-            ProblemDetail problemDetail = ProblemDetail
-                    .forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                            this.messageSource.getMessage("errors.400.title", new Object[0],
-                                    "errors.400.title", locale));
-            problemDetail.setProperty("errors",
-                    bindingResult.getAllErrors().stream()
-                            .map(MessageSourceResolvable::getDefaultMessage)
-                            .toList());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+            if (bindingResult instanceof BindException bindException) {
+                throw bindException;
+            } else {
+                throw new BindException(bindingResult);
+            }
         } else {
             this.productService.updateProduct(productId, payload.title(), payload.details());
             return ResponseEntity.noContent()
