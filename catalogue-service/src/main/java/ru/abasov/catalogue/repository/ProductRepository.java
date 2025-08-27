dcadb068a5592f883e05f14dfb669295a862ff13
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-08-28 01:11:26 +0300

    [catalogue-service] Запросы JPQL. Добавлена аннотация Query к методу findAllByTitleLikeIgnoreCase и использован JPQL стиль написания запроса

--- catalogue-service/src/main/java/ru/abasov/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ru/abasov/catalogue/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package ru.abasov.catalogue.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.abasov.catalogue.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    Iterable<Product> findAllByTitleLikeIgnoreCase(String filter);
+    @Query(value = "select p from Product p where p.title ilike :filter")
+    Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
