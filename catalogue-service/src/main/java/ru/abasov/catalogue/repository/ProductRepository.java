ecc409542fc12190e79a4fec903ba7cc90f6845b
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-08-28 01:17:45 +0300

    [catalogue-service] Native запрос.
    - Добавлен параметр nativeQuery=true
    - использован нативный запрос к базе данных

--- catalogue-service/src/main/java/ru/abasov/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ru/abasov/catalogue/repository/ProductRepository.java
@@ -7,6 +7,6 @@ import ru.abasov.catalogue.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    @Query(value = "select p from Product p where p.title ilike :filter")
+    @Query(value = "select * from catalogue.t_product where c_title ilike :filter", nativeQuery = true)
     Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
