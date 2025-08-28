79caa257920fbb58f1b1ab49b5835c4fc6c0e05f
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-08-29 00:43:47 +0300

    [catalogue-service] Рефакторинг метода productsRestClient для межсервисного взаимодействия используя Basic Authentication.
    - Добавление username, password
    - Добавление перехватчика запроса BasicAuthenticationInterceptor

--- manager-app/src/main/java/ru/abasov/manager/config/ClientBeans.java
+++ manager-app/src/main/java/ru/abasov/manager/config/ClientBeans.java
@@ -3,6 +3,7 @@ package ru.abasov.manager.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 import ru.abasov.manager.client.RestClientProductsRestClient;
 
@@ -11,9 +12,14 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductsRestClient productsRestClient(
-            @Value("${stockmaster.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri) {
+            @Value("${stockmaster.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            @Value("${stockmaster.services.catalogue.username:}") String catalogueUsername,
+            @Value("${stockmaster.services.catalogue.password:}") String cataloguePassword) {
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword)
+                )
                 .build());
     }
 }
