fbda930fbc5cbb678f5bebf2ddb9d26f4137dc0e
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-08-29 00:37:37 +0300

    [catalogue-service] Настройка SecurityFilterChain.
    - Настройка basic authentication
    - Отключение поддержки http сессий
    - Добавление кастомных name, password, roles

---
+++ catalogue-service/src/main/java/ru/abasov/catalogue/config/SecurityBeans.java
@@ -0,0 +1,26 @@
+package ru.abasov.catalogue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityBeans {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests.requestMatchers("/catalogue-api/**")
+                                .hasRole("SERVICE"))
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
