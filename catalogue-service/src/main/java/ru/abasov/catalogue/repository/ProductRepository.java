11b37f00d7438c1459fc55a68b1b9123f8ac071b
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-08-28 17:21:52 +0300

    [manager-app, catalogue-service] Добавлен поиск с фильтрацией.
    - В шаблоне list добавлена форма поиска с фильтрацией
    - Выбран JPQL запрос для поиска с фильтрацией
    - конкатенация с % добавлена из DefaultProductService в сам JPQL запрос ProductRepository

--- catalogue-service/src/main/java/ru/abasov/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ru/abasov/catalogue/repository/ProductRepository.java
@@ -7,6 +7,6 @@ import ru.abasov.catalogue.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    @Query(name = "Product.findAllByTitleLikeIgnoringCase")
+    @Query(value = "select p from Product p where p.title ilike concat('%', :filter, '%')")
     Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
