61b7cef77cf6197667e0e9c47c81aa171f9f4185
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-08-29 14:07:28 +0300

    [catalogue-service] [FIX] 401 Unauthorized
    Было: При запросах кроме get выдавало ошибку 401.
    Стало: Отключил csrf в securityFilterChain.

--- catalogue-service/src/main/java/ru/abasov/catalogue/config/SecurityBeans.java
+++ catalogue-service/src/main/java/ru/abasov/catalogue/config/SecurityBeans.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,8 +16,10 @@ public class SecurityBeans {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests.requestMatchers("/catalogue-api/**")
+                        authorizeHttpRequests
+                                .requestMatchers("/catalogue-api/**")
                                 .hasRole("SERVICE"))
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->
