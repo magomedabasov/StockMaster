8f468884845456c2d367aef1ffcf0e04821d25c1
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-09-06 02:27:52 +0300

    [manager-app] Добавление oAuth2UserService:
    преобразовывает параметр groups от keycloak в список прав пользователя

--- manager-app/src/main/java/ru/abasov/manager/config/SecurityBeans.java
+++ manager-app/src/main/java/ru/abasov/manager/config/SecurityBeans.java
@@ -4,7 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
 public class SecurityBeans {
@@ -16,5 +25,20 @@ public class SecurityBeans {
                         .anyRequest().hasRole("MANAGER"))
                 .oauth2Login(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
+        OidcUserService oidcUserService = new OidcUserService();
+        return userRequest -> {
+            OidcUser oidcUser = oidcUserService.loadUser(userRequest);
+            List<SimpleGrantedAuthority> authorities = Optional.ofNullable(oidcUser.getClaimAsStringList("groups"))
+                    .orElseGet(List::of)
+                    .stream()
+                    .filter(role -> role.contains("ROLE_"))
+                    .map(SimpleGrantedAuthority::new)
+                    .toList();
+            return new DefaultOidcUser(authorities, oidcUser.getIdToken());
+        };
     }
 }
