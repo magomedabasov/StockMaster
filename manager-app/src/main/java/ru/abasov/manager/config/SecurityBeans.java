91b629b080f122ad4643fdc5d3062ddc423c9fcd
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-09-06 00:16:06 +0300

    [manager-app] Настройка SecurityFilterChain

---
+++ manager-app/src/main/java/ru/abasov/manager/config/SecurityBeans.java
@@ -0,0 +1,20 @@
+package ru.abasov.manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().hasRole("MANAGER"))
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
