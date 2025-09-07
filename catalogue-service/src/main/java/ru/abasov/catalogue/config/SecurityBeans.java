b6f931b3e79eb01052debc899e873eb1938e9e1f
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-09-08 00:57:36 +0300

    [catalogue-service] Рефакторинг под oauth2

--- catalogue-service/src/main/java/ru/abasov/catalogue/config/SecurityBeans.java
+++ catalogue-service/src/main/java/ru/abasov/catalogue/config/SecurityBeans.java
@@ -2,28 +2,34 @@ package ru.abasov.catalogue.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityBeans {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(CsrfConfigurer::disable)
-                .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests
-                                .requestMatchers("/catalogue-api/**")
-                                .hasRole("SERVICE"))
-                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/catalogue-api/products")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.PATCH, "/catalogue-api/products/{productId.\\d}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.DELETE, "/catalogue/products/{productId.\\d}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.GET)
+                        .hasAuthority("SCOPE_view_catalogue")
+                        .anyRequest().denyAll())
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->
                         httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer ->
+                        httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults()))
                 .build();
     }
 }
