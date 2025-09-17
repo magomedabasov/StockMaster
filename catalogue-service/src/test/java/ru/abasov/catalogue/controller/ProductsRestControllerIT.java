a8685c0b96709fe07e95d5fd82a457d4c35a67dc
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-09-18 00:46:45 +0300

    [catalogue-service] Добавить интеграционные тесты.
    ProductsRestController:
    - findProducts с фильтрацией

---
+++ catalogue-service/src/test/java/ru/abasov/catalogue/controller/ProductsRestControllerIT.java
@@ -0,0 +1,47 @@
+package ru.abasov.catalogue.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductsRestControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void findProducts_ReturnsProductsList() throws Exception {
+        //given
+        var mockHttpServletRequestBuilder = MockMvcRequestBuilders.get("catalogue-api/products")
+                .param("filter", "товар")
+                .with(jwt().jwt(builder -> builder.claim("scope", "view_catalogue")));
+
+        //when
+        mockMvc.perform(mockHttpServletRequestBuilder)
+
+        //then
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
+                        content().json("""
+                                [
+                                {"id": 1, "title": "Товар №1", "details":  "Описание товара №1"},
+                                {"id": 2, "title": "Товар №2", "details":  "Описание товара №2"}
+                                ]
+                                """
+                        )
+                );
+    }
+}
