4e855d6c1ff7a294ee9eedd88a390c2886bd400a
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-09-27 01:17:03 +0300

    [manager-app] Добавить WireMock для тестирования:
    Ситуация:
    - невозможно поднять сервис для тестирования у себя или в докере по тем или иным причинам и тогда мы мокаем его через wiremock
    
    - Добавляем метод getProductsList_ReturnsProductsListPage() и имитируем ответ от catalogue-service через WireMock
    
    - создаём бин заглушку restClientProductsRestClient который будет обращаться по адресу localhost:54321 где у нас поднят мок catalogue-service
    
    - используем аннотацию Primary у бина restClientProductsRestClient, что бы был приоритет при внедрении и обойти NoUniqueBeanDefinitionException.

--- manager-app/src/test/java/ru/abasov/manager/config/TestBeans.java
+++ manager-app/src/test/java/ru/abasov/manager/config/TestBeans.java
@@ -1,9 +1,13 @@
 package ru.abasov.manager.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.web.client.RestClient;
+import ru.abasov.manager.client.RestClientProductsRestClient;
 
 import static org.mockito.Mockito.mock;
 
@@ -18,5 +22,15 @@ public class TestBeans {
     @Bean
     public OAuth2AuthorizedClientRepository authorizedClientRepository() {
         return mock(OAuth2AuthorizedClientRepository.class);
+    }
+
+    @Bean
+    @Primary
+    public RestClientProductsRestClient restClientProductsRestClient(
+            @Value("${stockmaster.services.catalogue.uri:http://localhost:54321}") String catalogueBaseUrl)
+    {
+        return new RestClientProductsRestClient(RestClient.builder()
+                .baseUrl(catalogueBaseUrl)
+                .build());
     }
 }
