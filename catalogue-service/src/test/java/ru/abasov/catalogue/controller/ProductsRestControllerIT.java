11d17e1c95b64a5fd611b534edc820bfc31968d7
magomedabasov <133795055+magomedabasov@users.noreply.github.com>
2025-09-28 02:37:39 +0300

    [catalogue-service] Добавить интеграционные тесты для repository.
    - @AutoConfigureTestDatabase - отключение создания тестовой базы данных. Вместо него используем бд catalogue.
    - @ActiveProfiles - используем для указания нужного yaml для каждого тестируемого компонента

--- catalogue-service/src/test/java/ru/abasov/catalogue/controller/ProductsRestControllerIT.java
+++ catalogue-service/src/test/java/ru/abasov/catalogue/controller/ProductsRestControllerIT.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("testcontainers")
 class ProductsRestControllerIT {
 
     @Autowired
